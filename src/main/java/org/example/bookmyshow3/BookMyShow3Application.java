c21fb80c48af47f78e4275e49f54ec2fe81f1041
Vasanth Kalluru <vasanthkalluru3519@gmail.com>
2025-01-01 21:58:28 +0530

    added auto-generation of id and auto-increment of updated at and modified at fields

--- src/main/java/org/example/bookmyshow3/BookMyShow3Application.java
+++ src/main/java/org/example/bookmyshow3/BookMyShow3Application.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookMyShow3Application implements CommandLineRunner {
     @Autowired
     private UserController userController;
--- src/main/java/org/example/bookmyshow3/models/BaseModel.java
+++ src/main/java/org/example/bookmyshow3/models/BaseModel.java
@@ -1,21 +1,28 @@
 package org.example.bookmyshow3.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseModel {
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreatedDate
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date createdAt;
+
+    @LastModifiedDate
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date updatedAt;
 }
